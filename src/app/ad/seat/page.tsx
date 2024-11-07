7a6cea2429bc25018cc8a45d4346d68351f8c92c
BlueJava2003 <tphuongnam98@gmail.com>
2024-11-08 00:08:11 +0700

    UI and handle API Seat and Seat type

---
+++ src/app/ad/seat/page.tsx
@@ -0,0 +1,10 @@
+import SeatManagement from "@/components/ad/seat/AdminSeat";
+import React from "react";
+
+export default function SeatPage() {
+  return (
+    <div>
+      <SeatManagement />
+    </div>
+  );
+}
