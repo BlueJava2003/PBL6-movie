ada835d31b79c6fe4e0a711cf84c2d8b69a8e84c
BlueJava2003 <tphuongnam98@gmail.com>
2024-11-16 13:26:11 +0700

    QR Code for ticket information

--- src/components/ticket/ticket.tsx
+++ src/components/ticket/ticket.tsx
@@ -1,76 +1,76 @@
-'use client';
-import Link from 'next/link';
-import { useRouter, useSearchParams } from 'next/navigation';
-import { useQRCode } from 'next-qrcode';
-import { useUserContext } from '@/lib/user.wrapper';
-import { useState } from 'react';
+"use client";
+
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+import { useQRCode } from "next-qrcode";
+import { useUserContext } from "@/lib/user.wrapper";
+import { useState, useMemo } from "react";
 
 const Ticket = () => {
   const searchParams = useSearchParams();
   const { Image } = useQRCode();
   const { infoBooking, setInfoBooking } = useUserContext() as IBookingContext;
   const [infoBookingObj, setInfoBookingObj] = useState<IInfoBooking>(
-    JSON.parse(localStorage.getItem('infoBooking') as string)
+    JSON.parse(localStorage.getItem("infoBooking") as string)
   );
+
+  // Function to generate ticket information URL for QR code
+  const generateTicketQRUrl = useMemo(() => {
+    const ticketData = {
+      movieName: infoBookingObj.movieName,
+      date: infoBookingObj.date,
+      time: `${infoBookingObj.timeStart}-${infoBookingObj.timeEnd}`,
+      seats: infoBookingObj.seatNames?.join(", "),
+      room: infoBookingObj.roomName,
+      price: infoBookingObj.price,
+    };
+    const encodedData = encodeURIComponent(JSON.stringify(ticketData));
+    return `http://192.168.1.19:3000/ticket-info?data=${encodedData}`;
+  }, [infoBookingObj]);
 
   return (
     <div className="max-w-screen-lg w-full px-6 lg:mx-auto lg:min-h-[500px] h-dvh my-10">
       <div className="p-[10px] bg-[#7e8eaa] uppercase text-white font-medium text-lg text-center md:w-2/3 lg:w-1/2 mx-auto">
         Thông tin vé
       </div>
-      {/* <div className="text-[21px] text-[#333] font-semibold pt-[30px] pb-[10px]">
-        Nội dung thanh toán
-      </div> */}
       <div className="border border-solid border-[#7e8eaa] px-8 py-5 md:w-2/3 lg:w-1/2 mx-auto">
         <div className="flex justify-center mb-6">
           <Image
-            text="https://github.com/Bunlong/next-qrcode"
+            text={generateTicketQRUrl}
             options={{
-              errorCorrectionLevel: 'L',
-              margin: 2,
-              scale: 5,
-              width: 150,
+              errorCorrectionLevel: "M",
+              margin: 3,
+              scale: 4,
+              width: 200,
               color: {
-                dark: '#000000',
-                light: '#ffffff',
+                dark: "#000000",
+                light: "#ffffff",
               },
             }}
           />
         </div>
         <p className="pb-[10px]">
-          <span className="text-[18px] text-[#0e1d2f] font-medium w-28 inline-block">
-            Phim:
-          </span>
+          <span className="text-[18px] text-[#0e1d2f] font-medium w-28 inline-block">Phim:</span>
           <span>{infoBookingObj.movieName}</span>
         </p>
         <p className="pb-[10px]">
-          <span className="text-[18px] text-[#0e1d2f] font-medium w-28 inline-block">
-            Ngày:
-          </span>
+          <span className="text-[18px] text-[#0e1d2f] font-medium w-28 inline-block">Ngày:</span>
           <span>{infoBookingObj.date}</span>
         </p>
         <p className="pb-[10px]">
-          <span className="text-[18px] text-[#0e1d2f] font-medium w-28 inline-block">
-            Thời gian:
-          </span>
+          <span className="text-[18px] text-[#0e1d2f] font-medium w-28 inline-block">Thời gian:</span>
           <span>{`${infoBookingObj.timeStart}-${infoBookingObj.timeEnd}`}</span>
         </p>
         <p className="pb-[10px]">
-          <span className="text-[18px] text-[#0e1d2f] font-medium w-28 inline-block">
-            Ghế:
-          </span>
-          <span>{infoBookingObj.seatNames?.join()}</span>
+          <span className="text-[18px] text-[#0e1d2f] font-medium w-28 inline-block">Ghế:</span>
+          <span>{infoBookingObj.seatNames?.join(", ")}</span>
         </p>
         <p className="pb-[10px]">
-          <span className="text-[18px] text-[#0e1d2f] font-medium w-28 inline-block">
-            Phòng chiếu:
-          </span>
+          <span className="text-[18px] text-[#0e1d2f] font-medium w-28 inline-block">Phòng chiếu:</span>
           <span>{infoBookingObj.roomName}</span>
         </p>
         <p className="pb-[10px]">
-          <span className="text-[18px] text-[#0e1d2f] font-medium w-28 inline-block">
-            Tổng tiền:
-          </span>
+          <span className="text-[18px] text-[#0e1d2f] font-medium w-28 inline-block">Tổng tiền:</span>
           <span>{infoBookingObj.price} VNĐ</span>
         </p>
       </div>
